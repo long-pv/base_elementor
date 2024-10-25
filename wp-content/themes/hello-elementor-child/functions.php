fafb69040efac000f6acc633b2c9ad3122d2e12a
long.phamvan <long05062000@gmail.com>
2024-10-25 14:35:10 +0700

    fixbug URI

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -1,5 +1,6 @@
 <?php
-define('CHILD_PATH', get_stylesheet_directory_uri());
+define('CHILD_URI', get_stylesheet_directory_uri());
+define('CHILD_PATH', get_stylesheet_directory());
 if (!defined('_S_VERSION')) {
     define('_S_VERSION', '1.0.0');
 }
@@ -9,11 +10,11 @@ if (!defined('_S_VERSION')) {
  */
 function base_elementor_scripts()
 {
-    wp_enqueue_style('child-theme-style', CHILD_PATH, array(), _S_VERSION);
+    wp_enqueue_style('child-theme-style', CHILD_URI, array(), _S_VERSION);
 
     // add custom main css/js
-    wp_enqueue_style('child-theme-style-main', CHILD_PATH . '/assets/css/main.css', array(), _S_VERSION);
-    wp_enqueue_script('child-theme-script-main', CHILD_PATH . '/assets/js/main.js', array(), _S_VERSION, true);
+    wp_enqueue_style('child-theme-style-main', CHILD_URI . '/assets/css/main.css', array(), _S_VERSION);
+    wp_enqueue_script('child-theme-script-main', CHILD_URI . '/assets/js/main.js', array(), _S_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'base_elementor_scripts');
 
@@ -26,3 +27,18 @@ function load_custom_widgets()
     require_once CHILD_PATH . '/elementor-widgets/index.php';
 }
 add_action('elementor/init', 'load_custom_widgets');
+
+// Setup theme setting page
+if (function_exists('acf_add_options_page')) {
+    $name_option = 'Theme Settings';
+    acf_add_options_page(
+        array(
+            'page_title' => $name_option,
+            'menu_title' => $name_option,
+            'menu_slug' => 'theme-settings',
+            'capability' => 'edit_posts',
+            'redirect' => false,
+            'position' => 80
+        )
+    );
+}
