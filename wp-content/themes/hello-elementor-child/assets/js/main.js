56a71525dc0b3232ce1a99672d37a0006010d5a7
longpv <long05062000@gmail.com>
2024-11-29 08:15:44 +0700

    js main

--- wp-content/themes/hello-elementor-child/assets/js/main.js
+++ wp-content/themes/hello-elementor-child/assets/js/main.js
@@ -1,25 +1,34 @@
 (function ($, window) {
-	// Open and close the mobile menu
-	$(".header .header__toggleItem").on("click", function () {
-		menu_open_sp();
-	});
+	var main_js = function ($scope, $) {
+		// Open and close the mobile menu
+		$(".header .header__toggleItem").on("click", function () {
+			menu_open_sp();
+		});
 
-	function menu_open_sp() {
-		$("body").toggleClass("mobile-menu-open");
-		$(".header__menusp").toggleClass("active");
-		$(".header__toggleItem").toggle();
-	}
-
-	$(".menu-item-has-children > .dropdown-arrow").click(function (e) {
-		e.preventDefault();
-		var $submenu = $(this).siblings(".sub-menu");
-
-		if ($submenu.length) {
-			$submenu.stop(true, true).slideToggle();
-			$(this).parent().toggleClass("open");
-			$(".sub-menu").not($submenu).slideUp();
-			$(".menu-item-has-children").not($(this).parent()).removeClass("open");
+		function menu_open_sp() {
+			$("body").toggleClass("mobile-menu-open");
+			$(".header__menusp").toggleClass("active");
+			$(".header__toggleItem").toggle();
 		}
+
+		$(".menu-item-has-children > .dropdown-arrow").click(function (e) {
+			e.preventDefault();
+			var $submenu = $(this).siblings(".sub-menu");
+
+			if ($submenu.length) {
+				$submenu.stop(true, true).slideToggle();
+				$(this).parent().toggleClass("open");
+				$(".sub-menu").not($submenu).slideUp();
+				$(".menu-item-has-children").not($(this).parent()).removeClass("open");
+			}
+		});
+		// end mobile menu
+	};
+
+	// tích hợp thêm vào trong elementor
+	$(window).on("elementor/frontend/init", function () {
+		elementorFrontend.hooks.addAction("frontend/element_ready/global", function ($scope, $) {
+			main_js($scope, $);
+		});
 	});
-	// end mobile menu
 })(jQuery, window);
