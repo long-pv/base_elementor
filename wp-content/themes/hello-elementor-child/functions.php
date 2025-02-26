d17f1d8fada60ddcb69168d1df6b23958bd8b32c
longpv <long05062000@gmail.com>
2025-02-26 23:27:42 +0700

    tự động cập nhật

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -6,9 +6,14 @@ if (!defined('_S_VERSION')) {
     define('_S_VERSION', '1.0.0');
 }
 
-// turn off auto update core wp
-define('AUTOMATIC_UPDATER_DISABLED', true);
-define('WP_AUTO_UPDATE_CORE', false);
+// turn on auto update core wp
+define('WP_AUTO_UPDATE_CORE', true); // Bật cập nhật tự động WordPress
+define('AUTOMATIC_UPDATER_DISABLED', false); // Đảm bảo cập nhật tự động không bị tắt
+define('WP_AUTO_UPDATE_PLUGINS', true); // Kích hoạt cập nhật tự động cho Plugin
+define('WP_AUTO_UPDATE_THEMES', true); // Kích hoạt cập nhật tự động cho Theme
+add_filter('auto_update_plugin', '__return_true'); // Tự động cập nhật plugin
+add_filter('auto_update_theme', '__return_true'); // Tự động cập nhật theme
+add_filter('auto_update_core', '__return_true'); // Tự động cập nhật WordPress core (cả bản lớn & nhỏ)
 
 /**
  * Enqueue scripts and styles.
