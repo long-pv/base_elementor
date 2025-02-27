c8a0b72fc579966d7fbba25d6bc12b6a07b98447
longpv <long05062000@gmail.com>
2025-02-27 11:39:03 +0700

    thêm đoạn code luôn tắt tự cập nhật

--- wp-content/themes/hello-elementor-child/functions.php
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -7,13 +7,20 @@ if (!defined('_S_VERSION')) {
 }
 
 // turn on auto update core wp
-define('WP_AUTO_UPDATE_CORE', true); // Bật cập nhật tự động WordPress
-define('AUTOMATIC_UPDATER_DISABLED', false); // Đảm bảo cập nhật tự động không bị tắt
-define('WP_AUTO_UPDATE_PLUGINS', true); // Kích hoạt cập nhật tự động cho Plugin
-define('WP_AUTO_UPDATE_THEMES', true); // Kích hoạt cập nhật tự động cho Theme
-add_filter('auto_update_plugin', '__return_true'); // Tự động cập nhật plugin
-add_filter('auto_update_theme', '__return_true'); // Tự động cập nhật theme
-add_filter('auto_update_core', '__return_true'); // Tự động cập nhật WordPress core (cả bản lớn & nhỏ)
+// define('WP_AUTO_UPDATE_CORE', true); // Bật cập nhật tự động WordPress
+// define('AUTOMATIC_UPDATER_DISABLED', false); // Đảm bảo cập nhật tự động không bị tắt
+// define('WP_AUTO_UPDATE_PLUGINS', true); // Kích hoạt cập nhật tự động cho Plugin
+// define('WP_AUTO_UPDATE_THEMES', true); // Kích hoạt cập nhật tự động cho Theme
+// add_filter('auto_update_plugin', '__return_true'); // Tự động cập nhật plugin
+// add_filter('auto_update_theme', '__return_true'); // Tự động cập nhật theme
+// add_filter('auto_update_core', '__return_true'); // Tự động cập nhật WordPress core (cả bản lớn & nhỏ)
+
+// turn off auto update core wp
+define('AUTOMATIC_UPDATER_DISABLED', true); // Vô hiệu hóa mọi cập nhật tự động
+define('WP_AUTO_UPDATE_CORE', false); // Ngăn cập nhật core WordPress
+define('DISALLOW_FILE_MODS', true); // Ngăn chỉnh sửa file từ Admin
+define('DISALLOW_FILE_EDIT', true); // Tắt trình chỉnh sửa file trong Dashboard
+add_filter('auto_update_plugin', '__return_false'); // Ngăn cập nhật tự động plugin
 
 /**
  * Enqueue scripts and styles.
